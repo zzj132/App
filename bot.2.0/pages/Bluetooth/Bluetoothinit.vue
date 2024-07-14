1dead97085f0d728cf8083850a3326360053bae3
luyy <256929413@qq.com>
2024-07-14 14:09:24 +0800

    一些更改

--- bot.2.0/pages/Bluetooth/Bluetoothinit.vue
+++ bot.2.0/pages/Bluetooth/Bluetoothinit.vue
@@ -13,39 +13,52 @@
                         </view>
                     </view>
                 </scroll-view>
-                
-                <button @click="initBlue">初始化蓝牙</button>
-                <button @click="discovery">搜索附近蓝牙设备</button>
-				<button @click="getService">获取蓝牙服务</button>
-				<button @click="getCharacteristics">获取特征值</button>
-				<button @click="notify">开启消息监听</button>
-				<button @click="getConnectionState"> 监测连接状态</button>
+				<view>
+                <button class="button button-blue button-outline" @click="initBlue">初始化蓝牙</button>
+				<br/>
+                <button class="button button-yellow button-outline" @click="discovery">搜索附近蓝牙设备</button>
+				<br />
+				<view>
+					<input class="input" data-id="code" key="code" type="text" v-model="code" placeholder="请输入指令"/>
+				</view>
+				<br />
+				<button class="button button-outline button-green " @click="send(code)">发送指令</button>
+				</view>
 
     </view>
 </template>
 ​
 <script setup>
 	import { ref } from "vue"
+	
+	/* 
+	 变量依次为：
+	 所获取的蓝牙列表
+	 设备id
+	 服务id
+	 特征值
+	 指令
+	 */
+	
 	const blueDeviceList=ref([])
 	const bleDeviceid=ref('')
 	const bleServiceid=ref('')
 	const bleCharacteristicId=ref('')
+	const code=ref('')
 	
-	
+	//监测连接状态
 	function getConnectionState(){
 		uni.onBLEConnectionStateChange(res=>{
-			console.log(res)
 			if(res.connected){
-				console.log(res.deviceId,"正常连接")
+				console.log(res.deviceId,":正常连接")
 			}else{
-				console.log("连接断开")
+				console.log("蓝牙连接断开")
 			}
 		})
 	}
 	
-	// 【1】初始化蓝牙
+	//初始化蓝牙
 	function initBlue() {
-		uni.getAppAuthorizeSetting()
 	    uni.openBluetoothAdapter({
 	        success(res) {
 	            console.log('初始化蓝牙成功')
@@ -59,12 +72,11 @@
 	}
 	
 	
-	// 【2】开始搜寻附近设备
+	//开始搜寻附近设备
 	function discovery() {
 	    uni.startBluetoothDevicesDiscovery({
 	        success(res) {
 	            console.log('开始搜索')
-	            
 	            // 开启监听回调
 	            uni.onBluetoothDeviceFound(found)
 	        },
@@ -75,32 +87,42 @@
 	    })
 	}
 	 
-	// 【3】找到新设备就触发该方法
+	//找到新设备就触发该方法
 	function found(res) {
 	    console.log(res)
 	    blueDeviceList.value.push(res.devices[0])
 	} 
 	
 	
-	//要连接的蓝牙deviceid
-	
-	
-	//【4】蓝牙连接
+	//蓝牙连接
 	function connect(data){
+		getConnectionState()
 		bleDeviceid.value=data.deviceId
-		bleServiceid.value=data.serviceId
 		uni.createBLEConnection({
 			deviceId:bleDeviceid.value,
 			success(res){
+				console.log(res)
 				console.log("连接成功")
 			// 停止搜索
 	            stopDiscovery()
+			//获取服务
+				getService()
+			//获取特征值
+				getCharacteristics()
+				
+			//开启监听
+				notify()
+				
+				console.log("方法调用了吗？")
 	        },
 	        fail(err) {
 	            console.log('连接失败')
 	            console.error(err)
 	        }
 	    })
+		}
+		
+	//停止扫描
 	function stopDiscovery(){
 		uni.stopBluetoothDevicesDiscovery({
 				success(res){
@@ -113,12 +135,14 @@
 				}
 			}
 		)}
-		
+	
+	//获取服务
 	function getService(){
 		console.log('获取服务方法启动')
 		uni.getBLEDeviceServices({
 			deviceId:bleDeviceid.value,
 			success(res){
+				bleServiceid.value=res.services[0].uuid
 				console.log("获取服务成功")
 				console.log(res)
 			},
@@ -129,15 +153,18 @@
 		})
 	}
 	
-	//【7】获取特征值
+	//获取特征值
 	function getCharacteristics(){
 		console.log("获取特征值方法启动")
 		uni.getBLEDeviceCharacteristics({
 			deviceId:bleDeviceid.value,
-			serviceId:bleServiceid,
+			serviceId:bleServiceid.value,
 			success(res){
 				bleCharacteristicId.value=res.characteristics[0].uuid
 				console.log('获取特征值成功')
+				console.log(bleCharacteristicId.value,"值是否存在？")
+				console.log(bleDeviceid)
+				console.log(bleServiceid)
 				console.log(res)
 			},
 			fail(err){
@@ -146,13 +173,15 @@
 			}
 		})
 	}
-		
+	
+	
+	//开启监听
 	function notify(){
-		
 		uni.notifyBLECharacteristicValueChange({
-			deviceId:bleDeviceid,
-			serviceId:bleServiceid,
-			 characteristicId:bleCharacteristicId,
+			state: true,
+			deviceId:bleDeviceid.value,
+			serviceId:bleServiceid.value,
+			characteristicId:bleCharacteristicId.value,
 			 success(res){
 				 console.log("监听成功")
 				 console.log(res)
@@ -165,20 +194,38 @@
 		
 		})
 	}
-		
-	function listenValueChange(){
-		uni.onBLECharacteristicValueChange(res=>{
-			console.log(res)
-			console.log('消息变化')
-		})
+	
+	
+	function send(code) {
+	    // 向蓝牙设备发送一个0x00的16进制数据
+	    const buffer = new ArrayBuffer(code.length)
+	    const dataView = new DataView(buffer)
+	    // dataView.setUint8(0, 0)
+	    
+	    for (var i = 0; i < code.length; i++) {
+	      dataView.setUint8(i, code.charAt(i).charCodeAt())
+	    }
+	    
+	    uni.writeBLECharacteristicValue({
+	      deviceId: bleDeviceid.value, // 设备ID，在【4】里获取到
+	      serviceId: bleServiceid.value, // 服务UUID，在【6】里能获取到
+	      characteristicId: bleCharacteristicId.value, // 特征值，在【7】里能获取到
+	      value: buffer,
+	      success(res) {
+	        console.log(res)
+	      },
+	      fail(err) {
+	        console.error(err)
+	      }
+	    })
 	}
 		
 		
-		
-	}
+	
 </script>
 
-<style> .box {
+<style> 
+.box {
     width: 100%;
     height: 400rpx;
     box-sizing: border-box;
@@ -190,6 +237,51 @@
     padding: 10rpx;
     border-bottom: 1px solid #ccc;
 }
-button {
-    margin-bottom: 20rpx;
-} </style>
+/* 基础按钮样式 */
+.button {
+                      
+  margin: 5% 10% 5% 10%;
+  
+  padding: 10px 80px;
+  font-size: 16px;
+  
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: #fff;
+  border: none;
+  border-radius: 8px;
+  background-color: #66afe9;
+  
+}
+
+
+
+/* 按钮按下效果 */
+.button:active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
+
+
+
+
+/* 基础样式 */
+.input {
+  height: 100%;
+  width: 100%; /* 宽度100%以适应容器 */
+  padding: 12px 20px; /* 内边距 */
+  margin: 8px 0; /* 外边距 */
+  box-sizing: border-box; /* 包括padding和border在width和height内 */
+  border: 2px solid #ccc; /* 边框样式 */
+  border-radius: 4px; /* 圆角 */
+  background-color: #f8f8f8; /* 背景颜色 */
+  font-size: 16px; /* 字体大小 */
+  color: #333; /* 文字颜色 */
+  transition: all 0.3s ease; /* 过渡动画 */
+}
+
+
+
+</style>
